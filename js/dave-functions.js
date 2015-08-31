dfaae38edacc2450cb14342211114371192ec3ea
Dave Gipp <djgipp@mac.com>
2015-08-30 17:49:21 -0700

    Preparing to store data in object named after file which was parsed

--- js/dave-functions.js
+++ js/dave-functions.js
@@ -8,11 +8,11 @@ nameParserHolder = {
  *
  * @author {Dave Gipp}
  * @constructor
- * @param {instance} The DOM ID which is invoking the function.
+ * @param {instance} The DOM element which is invoking the function.
  */
   init_state: function (instance){
     // do things to setup our particular instance. i.e. perhaps we have two parsers on the same page with different input file lists.
-    // also likely store parsed ata here for easy retrieval.
+    // also likely store parsed data here and not within the DOM for easy retrieval and to keep our separation of DOM and state (data).
     
     var fileList = jQuery("#" + instance).data('filelist');
     //NOTE: data attributes can only be retrieved as lowercase in recent jQuery.
@@ -26,7 +26,7 @@ nameParserHolder = {
      // create named element per instance containing data object
      nameParserHolder.instanceData[instance]={
      files:fileList
-     };//store instance data in Object, do not hang off DOM element
+     };//store instance data in name parser Object, do not hang off DOM element
     },
     /**
      * Load list of files and invoke parsing. Chaining from default ajax success method
@@ -39,10 +39,9 @@ nameParserHolder = {
     loadFiles: function (instance, fileList){
 
         var files=fileList.split(",");
-
         jQuery.get(files[0],
         function(data){
-          nameParserHolder.parseFiles(instance, data);
+          nameParserHolder.parseFiles(instance, data, files[0]);
         });
      },
      /**
@@ -52,7 +51,7 @@ nameParserHolder = {
      * @param {instance} The DOM ID which is invoking the function.
      * @param {fileList Object} list of files to load
      */
-    parseFiles: function (instance, data){
+    parseFiles: function (instance, data, filename){
         // find instance in array and merge data into object
         jQuery.extend(nameParserHolder.instanceData[instance],{
             //store raw data
@@ -71,10 +70,13 @@ nameParserHolder = {
             tmp = element.split(' ');
             tmpData[index] = tmp;
         });
-        // store clean data
-        jQuery.extend(nameParserHolder.instanceData[instance],{
-            cleanData: tmpData
-        });
+        // store clean data under object named according to file name that was parsed
+        dataTargetLocation = nameParserHolder.instanceData[instance];
+        // each file has data stored under file name
+        dataInjectedPerFilename = {};
+        dataInjectedPerFilename[filename] = tmpData;
+
+        jQuery.extend(dataTargetLocation,dataInjectedPerFilename);
 
     console.log(nameParserHolder.instanceData[instance]);
 
