b584693727c18268b4fb55575572921268ed488a
djgipp <djgipp@mac.com>
2015-08-31 17:05:10 -0700

    cleaner inline progress, better formatting and commenting

--- js/dave-functions.js
+++ js/dave-functions.js
@@ -1,36 +1,33 @@
 nameParserHolder = {
-  //global instance vars
+
   instanceData: [],
 
-/**
- * Access init state function.
- *
- * @author {Dave Gipp}
- * @constructor
- * @param {instance} The DOM element which is invoking the function.
- */
-  init_state: function (instance){
-    // do things to setup our particular instance. i.e. perhaps we have two parsers on the same page with different input file lists.
-    // also likely store parsed data here and not within the DOM for easy retrieval and to keep our separation of DOM and state (data).
-    
-    var fileList = jQuery("#" + instance).data('filelist');
-    //NOTE: data attributes can only be retrieved as lowercase in recent jQuery.
-    if (fileList){
-        jQuery("#"+instance).append("On instance name:" + instance + ", I found these files to load:" + fileList + "<br>");
-    
-       
-     } else {
-      // maybe throw up a place to drag files to be parsed if there's nothing in the data element
-     }
-     // create named element per instance containing a place to store our raw and clean data.
-     nameParserHolder.instanceData[instance]={
-     files:fileList
-     };
-    nameParserHolder.loadFilesWithPromise(instance);
+    /**
+     * Set up instance and prepare for loading source files to analyze
+     *
+     * @author {Dave Gipp}
+     * @constructor
+     * @param {instance} The DOM element which is invoking the function.
+     */
+    init_state: function (instance){
+
+        var fileList = jQuery("#" + instance).data('filelist');
+        //NOTE: data attributes can only be retrieved as lowercase in recent jQuery.
+        if (fileList){
+           jQuery("#"+instance).append("On instance name:" + instance + ", I found these files to load:" + fileList + "<br>");
+        } else {
+            //TODO: throw up a place to drag files to be parsed if there's nothing in the data element
+        }
+
+        nameParserHolder.instanceData[instance]={
+            files:fileList
+        };
+
+        nameParserHolder.loadFilesWithPromise(instance);
     },
     /**
-     * Load list of files and invoke parsing. Chaining from default ajax success method
-     * is the only reliable way to avoid race conditions.
+     * Load list of files via ajax. Set promises and wait for success. Invoke parsing
+     * 
      *
      * @author {Dave Gipp}
      * @param {instance} The DOM ID which is invoking the function.
@@ -40,15 +37,15 @@ nameParserHolder = {
         
         var files=nameParserHolder.instanceData[instance].files.split(",");
         var promiseAllFilesAreLoaded = jQuery.Deferred();
-        // create area to store raw data, to be used once per retreived file.
+
         var dataRetrievedPerFilename = {
-            rawdata:{}
+                rawdata:{}
             };
 
         var promiseRequests = jQuery.map(files, function(currentRequest){
             jQuery("#"+instance).append("dispatching get for file:" + currentRequest + "<br>");
             return jQuery.get(currentRequest,function (data){
-                //store raw data under current filename
+                //store raw data under object named after current filename
                 dataRetrievedPerFilename.rawdata[currentRequest] = data;
                 jQuery.extend(nameParserHolder.instanceData[instance], dataRetrievedPerFilename);
             });
@@ -61,7 +58,7 @@ nameParserHolder = {
 
      },
      /**
-     * Parse collected data into standard format. Preserve all data.
+     * Parse collected data into standard format. Preserve all data for future use.
      *
      * @author {Dave Gipp}
      * @param {instance} The DOM ID which is invoking the function.
@@ -69,7 +66,7 @@ nameParserHolder = {
     parseData: function (instance){
         var myCollections = nameParserHolder.instanceData[instance];
         var dataRetrievedPerFilename = {
-            cleandata:{}
+                cleandata:{}
             };
 
         jQuery.each(myCollections.rawdata, function(filename, rawdata) {
@@ -86,12 +83,19 @@ nameParserHolder = {
                 tmpData[index] = tmp;
             });
             dataRetrievedPerFilename.cleandata[filename] = tmpData;
+            // store clean data
             jQuery.extend(nameParserHolder.instanceData[instance], dataRetrievedPerFilename);
         });
 
      jQuery("#"+instance).append("Data parsing completed for instance:" + instance + "<br>");
      nameParserHolder.collateData(instance);
     },
+     /**
+     * Collate data by letter frequency. Add or increment into letter object.
+     *
+     * @author {Dave Gipp}
+     * @param {instance} The DOM ID which is invoking the function.
+     */
     collateData: function (instance){
         var letterObject = {
             letters:{}
@@ -114,7 +118,7 @@ nameParserHolder = {
             });
         });
         jQuery.extend(nameParserHolder.instanceData[instance], letterObject);
-        jQuery("#"+instance).append(JSON.stringify(letterObject));
+        jQuery("#"+instance).append(JSON.stringify(letterObject) + "<p></p>");
     },
     presentData: function (name) {
 
@@ -122,8 +126,8 @@ nameParserHolder = {
 };
 
 jQuery(document).ready(function(){
-  // instantiate every parser instance on page
-  jQuery(".nameParser").each(function(){
-    nameParserHolder.init_state(this.id);
-  });
+    // instantiate every parser div on page, which will invoke parsing for the files listed in data element
+    jQuery(".nameParser").each(function(){
+       nameParserHolder.init_state(this.id);
+    });
 });
