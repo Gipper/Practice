90c7ca5089fea56504b30eabad56169b34f499b0
djgipp <djgipp@mac.com>
2015-08-31 17:06:20 -0700

    woefully inadequate yet passing scripts

--- spec/solutionSpec.js
+++ spec/solutionSpec.js
@@ -4,7 +4,7 @@ describe("ParserTest", function() {
 
   beforeEach(function() {
     // setup generic parser div
-    parserTestDiv = '<div class="nameParser" id="parserTest"></div>';
+    parserTestDiv = '<div class="nameParserTest" id="parserTest"></div>';
     jQuery("#testHolder").append(parserTestDiv);
     // store init helpers
     testHelpers = {
@@ -30,6 +30,7 @@ describe("ParserTest", function() {
         // add data specific to this test
         jQuery("#parserTest").data('filelist',"male-first.txt,female-first.txt");
         testHelpers.initTests();
+        nameParserHolder.loadFilesWithPromise("parserTest");
       });
 
       afterEach(function() {
@@ -37,9 +38,15 @@ describe("ParserTest", function() {
         jQuery("#parserTest").removeData('filelist');
       });
 
-      it("should retrieve 2 items in data-element when 2 are provided", function() {
+      it("should discover 2 items in data-element when 2 are provided", function() {
         expect(jQuery("#parserTest").data('filelist').split(",").length).toEqual(2);
       });
+
+
+      it("should load two files asynchron when there are two", function() {
+        console.log();
+      });
+
   });
   
   describe("when loading 1 data element it should process one correctly", function() {
@@ -50,13 +57,13 @@ describe("ParserTest", function() {
         testHelpers.initTests();
       });
 
+      it("should discover 1 item in data-element when 1 is provided", function() {
+        expect(jQuery("#parserTest").data('filelist').split(",").length).toEqual(1);
+      });
+
       afterEach(function() {
         // remove specific data
         jQuery("#parserTest").removeData('filelist');
-      });
-
-      it("should retrieve 1 item in data-element when 1 is provided", function() {
-        expect(jQuery("#parserTest").data('filelist').split(",").length).toEqual(1);
       });
   });
 });
