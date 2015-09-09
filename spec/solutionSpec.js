024caa11727635afaf247037a2cfc6d4c68c5c17
djgipp <djgipp@mac.com>
2015-09-09 14:57:02 -0700

    Basic working integration tests around load method. Using returned promise to complete done before each test.

--- spec/solutionSpec.js
+++ spec/solutionSpec.js
@@ -2,21 +2,21 @@ describe("ParserTest", function() {
   var testHelpers = {};
   var parserTestDiv;
 
+  testHelpers = {
+    initTests: function (){
+      jQuery(document).ready(function(){
+        // instantiate test parser instance on page. will only be one.
+        jQuery("#parserTest").each(function(){
+          nameParserHolder.init_state(this.id);
+        });
+      });
+    }
+  };
+
   beforeEach(function() {
     // setup generic parser div
     parserTestDiv = '<div class="nameParserTest" id="parserTest"></div>';
     jQuery("#testHolder").append(parserTestDiv);
-    // store init helpers
-    testHelpers = {
-      initTests: function (){
-        jQuery(document).ready(function(){
-          // instantiate test parser instance on page. will only be one.
-          jQuery("#parserTest").each(function(){
-            nameParserHolder.init_state(this.id);
-          });
-        });
-      }
-    };
   });
 
  afterEach(function() {
@@ -26,11 +26,13 @@ describe("ParserTest", function() {
 
   describe("when loading data elements it should process two correctly", function() {
 
-      beforeEach(function() {
+      beforeEach(function(done) {
         // add data specific to this test
         jQuery("#parserTest").data('filelist',"male-first.txt,female-first.txt");
         testHelpers.initTests();
-        nameParserHolder.loadFilesWithPromise("parserTest");
+        nameParserHolder.loadFilesWithPromise("parserTest").then( function () {
+            done();
+        });
       });
 
       afterEach(function() {
@@ -43,22 +45,30 @@ describe("ParserTest", function() {
       });
 
 
-      it("should load two files asynchron when there are two", function() {
-        console.log();
+      it("should result in 2 raw data entries when 2 files are provided", function() {
+        expect(nameParserHolder.instanceData.parserTest.rawdata["female-first.txt"]).toBeDefined();
+        expect(nameParserHolder.instanceData.parserTest.rawdata["male-first.txt"]).toBeDefined();
       });
 
   });
   
   describe("when loading 1 data element it should process one correctly", function() {
 
-      beforeEach(function() {
+      beforeEach(function(done) {
         // add data specific to this test
         jQuery("#parserTest").data('filelist',"female-first.txt");
         testHelpers.initTests();
+        nameParserHolder.loadFilesWithPromise("parserTest").then( function () {
+            done();
+        });
       });
 
       it("should discover 1 item in data-element when 1 is provided", function() {
         expect(jQuery("#parserTest").data('filelist').split(",").length).toEqual(1);
+      });
+
+      it("should result in 1 raw data entries when 1 file is provided", function() {
+        expect(nameParserHolder.instanceData.parserTest.rawdata["female-first.txt"]).toBeDefined();
       });
 
       afterEach(function() {
